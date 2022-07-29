463e7bbd808241e2b5e4ca24fba6b2219fc47237
Thodler <neoskywalker01@gmail.com>
2022-07-29 11:28:20 +0200

    [FIX] Import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {PageInfoComponent} from "./page-info/page-info.component";
+import {RowComponent} from "./row/row.component";
+import {ListComponent} from "./list/list.component";
+import {MatSelectModule} from "@angular/material/select";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormAiportComponent} from "./pages/form-aiport/form-aiport.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     PageInfoComponent,
     RowComponent,
-    ListComponent
+    ListComponent,
+    FormAiportComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatToolbarModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/form-aiport/form-aiport.component.ts
+++ src/app/pages/form-aiport/form-aiport.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {AirportService} from "../../../services/airport.service";
+import {Observable} from "rxjs";
+import {Flight} from "../../flight.model";
 
+export class Airports {
+  states!: Flight[]
+}
 @Component({
   selector: 'app-form-aiport',
   templateUrl: './form-aiport.component.html',
@@ -7,9 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormAiportComponent implements OnInit {
 
-  constructor() { }
+  airports!: Observable<Airports>
+
+  constructor(
+    private airport: AirportService
+  ) { }
 
   ngOnInit(): void {
+    this.airports = this.getAirport()
   }
 
+  getAirport(){
+    return this.airport.getStateAll()
+  }
 }
