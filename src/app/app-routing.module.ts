07def82618d8a118a1f7dfef0c8ddf24a5bdaff0
Thodler <neoskywalker01@gmail.com>
2022-07-29 10:52:06 +0200

    [UPD] Route for Home(Airport)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FormAiportComponent} from "./pages/form-aiport/form-aiport.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: FormAiportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
