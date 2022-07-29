d409d4d8422fbd8ac5d129138ac6dda5026a70fe
Thodler <neoskywalker01@gmail.com>
2022-07-29 10:50:42 +0200

    [ADD] Component FormAirport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { FormAiportComponent } from './pages/form-aiport/form-aiport.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormAiportComponent
   ],
     imports: [
         BrowserModule,
---
+++ src/app/pages/form-aiport/form-aiport.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-aiport',
+  templateUrl: './form-aiport.component.html',
+  styleUrls: ['./form-aiport.component.scss']
+})
+export class FormAiportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
