e437db8abbfd727471d6984e85693a0dc9135225
Thodler <neoskywalker01@gmail.com>
2022-07-29 23:33:56 +0200

    [ADD] autocomplete.component preparation pour l'autocomplete dans le select du formulaire

---
+++ src/app/helpers/autocomplete/autocomplete.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {map, Observable, startWith} from "rxjs";
+import {FormControl} from "@angular/forms";
+
+@Component({
+  selector: 'app-autocomplete',
+  templateUrl: './autocomplete.component.html',
+  styleUrls: ['./autocomplete.component.scss']
+})
+export class AutocompleteComponent implements OnInit {
+
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> | undefined;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+}
