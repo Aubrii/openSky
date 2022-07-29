b11c814a400c0d4cd4b54c8bad3aab746f4dcd6b
Thodler <neoskywalker01@gmail.com>
2022-07-29 18:31:00 +0200

    [ADD] Airport Model

---
+++ src/models/airport.model.ts
@@ -0,0 +1,20 @@
+export class AirportModel {
+  carriers!: string
+  city!: string
+  code!: string
+  country!: string
+  direct_flights!: string
+  elev!: string
+  email!: string
+  icao!: string
+  lat!: string
+  lon!: string
+  name!: string
+  phone!: string
+  runway_length!: string
+  state!: string
+  type!: string
+  tz!: string
+  url!: string
+  woeid!: string
+}
